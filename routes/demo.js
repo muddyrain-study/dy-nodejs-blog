a1aef2df61798717a8d1d852d86bbb5df508c226
‘muddyrain’ <qiu3291002845@gmail.com>
2023-06-05 21:01:19 +0800

    feat: 项目管理

--- routes/demo.js
+++ routes/demo.js
@@ -1,17 +1,31 @@
 var express = require('express');
+const {
+  findAllDemoService,
+  addDemoService,
+  updateDemoService,
+  deleteDemoService,
+} = require('../service/demoService');
 
 var router = express.Router();
 
 // 获取项目
-router.get('/', async function (req, res, next) {});
+router.get('/', async function (req, res, next) {
+  res.send(await findAllDemoService());
+});
 
-// 新增 项目
-router.post('/', async function (req, res, next) {});
+// 新增项目
+router.post('/', async function (req, res, next) {
+  res.send(await addDemoService(req.body));
+});
 
-// 修改 项目
-router.put('/:id', async function (req, res, next) {});
+// 修改项目
+router.put('/:id', async function (req, res, next) {
+  res.send(await updateDemoService(req.params.id, req.body));
+});
 
-// 删除 项目
-router.delete('/:id', async function (req, res, next) {});
+// 删除项目
+router.delete('/:id', async function (req, res, next) {
+  res.send(await deleteDemoService(req.params.id));
+});
 
 module.exports = router;
